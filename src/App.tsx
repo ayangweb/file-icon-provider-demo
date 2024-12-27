22b7b9adb95cf0adb962ba2dd1ef540ce04b6784
ayang <473033518@qq.com>
2024-12-27 11:32:33 +0800

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { invoke } from "@tauri-apps/api/core";
+import { Flex, Image, Input, message } from "antd";
+import { useState } from "react";
+
+const App = () => {
+  const [url, setUrl] = useState("");
+
+  return (
+    <Flex vertical gap="middle">
+      <Input.Search
+        enterButton="Get Icon"
+        placeholder="Enter a path"
+        onSearch={async (value) => {
+          if (!value) {
+            return message.warning("Please enter a path");
+          }
+
+          const bytes = await invoke<Uint8Array>("get_icon", {
+            path: value,
+            size: 256,
+          });
+
+          const blob = new Blob([new Uint8Array(bytes)], { type: "image/png" });
+          const url = URL.createObjectURL(blob);
+          setUrl(url);
+        }}
+      />
+
+      {url && <Image src={url} width={128} />}
+    </Flex>
+  );
+};
+
+export default App;
